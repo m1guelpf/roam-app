78cdb35577209a62308a9053b02c3bbd5ce35ee0
Miguel Piedrafita <git@miguelpiedrafita.com>
2021-08-08 01:21:17 +0200

    wip

--- Roam Research/ViewController.swift
+++ Roam Research/ViewController.swift
@@ -6,14 +6,135 @@
 //
 
 import UIKit
+import WebKit
 
 class ViewController: UIViewController {
+	
+	let userDefaults: UserDefaults = .standard
 
 	override func viewDidLoad() {
 		super.viewDidLoad()
-		// Do any additional setup after loading the view.
+		
+		let subgraphSlug = userDefaults.value(forKey: "subgraphSlug") as? String?
+		
+		if (subgraphSlug ?? nil) != nil {
+			loadWebView(subgraphSlug: subgraphSlug!!)
+		}
 	}
+	
+	override func viewDidAppear(_ animated: Bool) {
+		let subgraphSlug = userDefaults.value(forKey: "subgraphSlug") as? String?
+		
+		if (subgraphSlug ?? nil) != nil {
+			return
+		}
 
+		let alertController = UIAlertController(title: "Roam Graph", message: "Enter the slug of your Roam graph", preferredStyle: .alert)
 
+		alertController.addTextField { (textField) in textField.placeholder = "m1guelpf" }
+
+		alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { [self] (action) in
+			if let text = alertController.textFields?.first?.text {
+				userDefaults.set(text as String?, forKey: "subgraphSlug")
+				loadWebView(subgraphSlug: text)
+			} else {
+				self.closeApp()
+			}
+		}))
+
+		present(alertController, animated: true, completion: nil)
+	}
+	
+	func loadWebView(subgraphSlug: String) {
+		let config = WKWebViewConfiguration()
+		let script = WKUserScript(source: "document.body.classList.add('roam-app')", injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+		config.userContentController.addUserScript(script)
+
+		let webView = WKWebView(frame: view.frame, configuration: config)
+		webView.navigationDelegate = self
+		webView.uiDelegate = self
+		webView.allowsBackForwardNavigationGestures = true
+		webView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+		view.addSubview(webView)
+		
+		let url = URL(string: "https://roamresearch.com/#/app/\(subgraphSlug)")!
+		let request = URLRequest(url: url)
+		webView.load(request)
+	}
+	
+	func closeApp() {
+		UIControl().sendAction(#selector(URLSessionTask.suspend), to: UIApplication.shared, for: nil)
+		DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(1), execute: {
+			exit(EXIT_SUCCESS)
+		})
+	}
 }
 
+// Handler for external links & loading errors
+extension ViewController: WKNavigationDelegate {
+	// Open external links on either their specific apps or Safari
+	func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> Void) {
+		if navigationAction.navigationType == .linkActivated  {
+			if let url = navigationAction.request.url,
+				UIApplication.shared.canOpenURL(url) {
+					UIApplication.shared.open(url)
+					decisionHandler(.cancel)
+			} else {
+				// Open in web view
+				decisionHandler(.allow)
+			}
+		} else {
+			// other navigation type, such as reload, back or forward buttons
+			decisionHandler(.allow)
+		}
+	}
+	
+	// Close app if loading Roam fails
+	func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+		let alertController = UIAlertController(title: nil, message: error.localizedDescription, preferredStyle: .alert)
+		alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in self.closeApp() }))
+
+		present(alertController, animated: true, completion: nil)
+	}
+}
+
+// Render alert(), confirm() & dialog()
+extension ViewController: WKUIDelegate {
+	func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void)
+	{
+		let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+		alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in completionHandler() }))
+
+		present(alertController, animated: true, completion: nil)
+	}
+	
+	func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+
+		let alertController = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+
+		alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in completionHandler(true) }))
+
+		alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action) in completionHandler(false) }))
+
+		present(alertController, animated: true, completion: nil)
+	}
+	
+	func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+
+		let alertController = UIAlertController(title: nil, message: prompt, preferredStyle: .alert)
+
+		alertController.addTextField { (textField) in textField.text = defaultText }
+
+		alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { (action) in
+			if let text = alertController.textFields?.first?.text {
+				completionHandler(text)
+			} else {
+				completionHandler(defaultText)
+			}
+		}))
+
+		alertController.addAction(UIAlertAction(title: "Cancel", style: .default, handler: { (action) in completionHandler(nil) }))
+
+		present(alertController, animated: true, completion: nil)
+	}
+}
---
+++ Share to Roam/ShareViewController.swift
@@ -0,0 +1,139 @@
+//
+//  ShareViewController.swift
+//  Share to Roam
+//
+//  Created by Miguel Piedrafita on 7/8/21.
+//
+
+import UIKit
+import WebKit
+
+class CustomShareViewController: UIViewController {
+	let userDefaults: UserDefaults = .standard
+
+	private var urlString: String? {
+		didSet {
+			reloadWebview()
+		}
+	}
+
+	private var textString: String? {
+		didSet {
+			reloadWebview()
+		}
+	}
+
+	private var webViewUrl: String {
+		let encodedText = textString?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+		let encodedUrl = urlString?.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? ""
+
+		return "https://roamresearch.com/?\(encodedText == "" ? "a" : "text=\(encodedText)")&\(encodedUrl == "" ? "b" : "url=\(encodedUrl)")#quick-capture"
+	}
+
+	private var webView: WKWebView?
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		ensureConfigured()
+		loadShareContent()
+
+		self.view.backgroundColor = .systemGray6
+		setupNavBar()
+		setupViews()
+	}
+	
+	private func ensureConfigured() {
+		let subgraphSlug = userDefaults.value(forKey: "subgraphSlug")
+
+		if (subgraphSlug ?? nil) == nil {
+			let alertController = UIAlertController(title: nil, message: "Please select a graph from the Roam app first.", preferredStyle: .alert)
+			alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { [self] (action) in self.cancelAction() }))
+
+			present(alertController, animated: true, completion: nil)
+		}
+
+	}
+	
+	private func loadShareContent() {
+		let extensionItem = extensionContext?.inputItems[0] as! NSExtensionItem
+	
+		for attachment in extensionItem.attachments! {
+		  if attachment.hasItemConformingToTypeIdentifier("public.url") {
+			attachment.loadItem(forTypeIdentifier: "public.url", options: nil, completionHandler: { (results, error) in
+			  let url = results as! URL?
+			  self.urlString = url!.absoluteString
+			})
+		  }
+		  if attachment.hasItemConformingToTypeIdentifier("public.plain-text") {
+			attachment.loadItem(forTypeIdentifier: "public.plain-text", options: nil, completionHandler: { (results, error) in
+			  let text = results as! String
+			  self.textString = text
+			})
+		  }
+		}
+		
+		
+	}
+
+	// 2: Set the title and the navigation items
+	private func setupNavBar() {
+		self.navigationItem.title = "Roam Research"
+
+		let itemCancel = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelAction))
+		self.navigationItem.setLeftBarButton(itemCancel, animated: false)
+
+		let itemDone = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneAction))
+		self.navigationItem.setRightBarButton(itemDone, animated: false)
+	}
+	
+	private func setupViews() {
+		self.webView = WKWebView(frame: view.frame)
+		self.webView!.navigationDelegate = self
+		self.webView!.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+		self.view.addSubview(self.webView!)
+
+		let request = URLRequest(url: URL(string: self.webViewUrl)!)
+		self.webView!.load(request)
+	}
+	
+	private func reloadWebview() {
+		self.webView?.load(URLRequest(url: URL(string: self.webViewUrl)!))
+	}
+
+	// 3: Define the actions for the navigation items
+	@objc private func cancelAction () {
+		let error = NSError(domain: "me.m1guelpf.RoamResearch", code: 0, userInfo: [NSLocalizedDescriptionKey: "User cancelled request"])
+		extensionContext?.cancelRequest(withError: error)
+	}
+
+	@objc private func doneAction() {
+		extensionContext?.completeRequest(returningItems: [], completionHandler: nil)
+	}
+}
+
+extension CustomShareViewController: WKNavigationDelegate {
+	// Close prompt if loading Roam fails
+	func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+		let alertController = UIAlertController(title: nil, message: error.localizedDescription, preferredStyle: .alert)
+		alertController.addAction(UIAlertAction(title: "OK", style: .default, handler: { [self] (action) in self.cancelAction() }))
+
+		present(alertController, animated: true, completion: nil)
+	}
+}
+
+@objc(CustomShareNavigationController)
+class CustomShareNavigationController: UINavigationController {
+
+	override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: Bundle?) {
+		super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+
+		// 2: set the ViewControllers
+		self.setViewControllers([CustomShareViewController()], animated: false)
+	}
+
+	@available(*, unavailable)
+	required init?(coder aDecoder: NSCoder) {
+		super.init(coder: aDecoder)
+	}
+}
